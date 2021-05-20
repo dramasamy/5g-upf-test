72811b17d8d5696bc6c4f7decc230440b0287ce8
Dinesh Kumar Ramasamy <dineshkumarr.ramasamy@gmail.com>
2021-05-19 22:18:00 -0500

    Adding QUIC echo server

--- isp/go.mod
+++ isp/go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/gin-contrib/static v0.0.1
 	github.com/gin-gonic/gin v1.7.1
+	github.com/lucas-clemente/quic-go v0.20.1
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 )
---
+++ isp/quic_client.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"flag"
+	"io"
+	"log"
+	"strconv"
+	"time"
+
+	quic "github.com/lucas-clemente/quic-go"
+)
+
+func main() {
+	hostName := flag.String("hostname", "localhost", "hostname/ip of the server")
+	portNum := flag.String("port", "4242", "port number of the server")
+	numEcho := flag.Int("necho", 100, "number of echos")
+	timeoutDuration := flag.Int("rtt", 50, "timeout duration (in ms)")
+
+	flag.Parse()
+
+	addr := *hostName + ":" + *portNum
+
+	tlsConf := &tls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"quic-echo"},
+	}
+
+	session, err := quic.DialAddr(addr, tlsConf, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	stream, err := session.OpenStreamSync(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	counter := 0
+	timeout := time.Duration(*timeoutDuration) * time.Millisecond
+
+	resp := make(chan string)
+
+	for {
+		message := strconv.Itoa(counter)
+		counter++
+
+		log.Printf("SENDING '%s'\n", message)
+		_, err = stream.Write([]byte(message))
+		if err != nil {
+			panic(err)
+		}
+
+		// log.Println("SENT, Waiting for echo")
+
+		go func() {
+			buff := make([]byte, len(message))
+			_, err = io.ReadFull(stream, buff)
+			if err != nil {
+				panic(err)
+			}
+
+			resp <- string(buff)
+		}()
+
+		select {
+		case reply := <-resp:
+			log.Printf("RECEIVING '%s'\n", reply)
+		case <-time.After(timeout):
+			log.Printf("TIMEOUT\n")
+		}
+
+		if counter == *numEcho {
+			break
+		}
+	}
+}
